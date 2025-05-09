6804509c993e4eafbd1fa01d7d1c7c19ad123ab7
Isaquellucas <isaquesilva7268@gmail.com>
2025-05-09 00:36:51 -0300

    meu primeiro commit :)

--- code.js
+++ code.js
@@ -1,1 +1,32 @@
-console.log ("ola mundo");
+let nomeDoHeroi = "Steve"
+let quantidadeDeXP = "1000"
+ 
+if (quantidadeDeXP === 1000){
+    console.log (nomeDoHeroi + " esta no nivel de prata")
+}
+else if (quantidadeDeXP <= 1000){
+    console.log (nomeDoHeroi + " ta com pouco XP ")
+}
+
+else if (quantidadeDeXP <= 2000){
+    console.log (nomeDoHeroi + " esta no nivel bronze")
+} 
+
+else if (quantidadeDeXP <= 5000){
+    console.log (nomeDoHeroi + " esta no nivel prata")
+}
+else if (quantidadeDeXP <= 7000){
+    console.log (nomeDoHeroi + " esta no nivel ouro")
+}
+else if (quantidadeDeXP <= 8000){
+    console.log (nomeDoHeroi + " esta no nivel platina")
+}
+else if (quantidadeDeXP <= 9000 ){
+    console.log (nomeDoHeroi + " esta no nivel ascendente ")
+}
+else if (quantidadeDeXP <= 10000){
+    console.log (nomeDoHeroi + " esta no nivel imortal ")
+}
+else if (quantidadeDeXP <= 10001){
+    console.log (nomeDoHeroi + " esta nivel radiante ")
+} 
